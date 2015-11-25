b4b21c2baf947bb437b2fa3e1323083ce87859ae
Anton Yabchinskiy <arn@bestmx.ru>
2015-11-25 23:14:04 +0300

    Import no-op cpu_temp status for Linux

---
+++ cpu_temp-linux.c
@@ -0,0 +1,14 @@
+/* License: WTFPL (http://www.wtfpl.net/) */
+
+#include "cpu_temp.h"
+
+void
+cpu_temp_update(void* opaque, time_t now, char* buf, size_t buf_sz)
+{
+    (void)opaque;
+    (void)now;
+
+    /* TODO */
+
+    snprintf(buf, buf_sz, "N/A");
+}
