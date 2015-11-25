f5a4d6d013ffa384ffd874d8a963b8692336792f
Anton Yabchinskiy <arn@bestmx.ru>
2015-11-25 16:58:10 +0300

    Move non-BSD make statements to a common.mk file

---
+++ common.mk
@@ -0,0 +1,12 @@
+OS !=	uname -s | tr '[A-Z]' '[a-z]'
+
+PROG =	dwmstatus
+OBJS =	battery-${OS}.o loadavg.o maildir.o main.o status.o time.o
+
+# Add these lines to build in stdout mode
+# CFLAGS +=	-D_NO_X11
+
+# Remove these lines to build in stdout mode
+CFLAGS +=	-I/usr/X11R6/include
+LDFLAGS +=	-L/usr/X11R6/lib
+LDADD +=	-lX11
