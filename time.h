3715e5a1db5f60bd44ba00e974db7022c0e88561
Anton Yabchinskiy <arn@bestmx.net>
2016-07-11 14:06:40 +0300

    time: strdup() time fmt string

--- time.h
+++ time.h
@@ -3,10 +3,13 @@
 #ifndef __TIME_H_INCLUDED__
 #define __TIME_H_INCLUDED__
 
+#include <stdlib.h>
+#include <string.h>
+
 #include "status.h"
 
-#define time_make(_interval, _format)                       \
-    status_make((_interval), (_format), NULL, time_update)
+#define time_make(_interval, _format)                               \
+    status_make((_interval), strdup(_format), free, time_update)
 
 void
 time_update(void* opaque, time_t now, char* buf, size_t buf_sz)
