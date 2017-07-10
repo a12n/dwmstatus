af64891819634c78210217fa57c8cdeba41faa5d
Anton Yabchinskiy <arn@bestmx.net>
2017-07-10 23:38:52 +0300

    Add utime status
    Depends on libicu.

---
+++ utime.h
@@ -0,0 +1,11 @@
+/* License: WTFPL (http://www.wtfpl.net/) */
+
+#ifndef __UTIME_H_INCLUDED__
+#define __UTIME_H_INCLUDED__
+
+#include "status.h"
+
+struct status
+utime_status(double interval, const char* format);
+
+#endif  /* __UTIME_H_INCLUDED__ */
