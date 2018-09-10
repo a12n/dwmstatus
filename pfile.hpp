6540794a39b754de3ddce07781a0d9d59e20b75d
Anton Yabchinskiy <arn@bestmx.net>
2018-09-10 23:31:25 +0300

    Pseudo file class to read from sysfs or proc

---
+++ pfile.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "decl.hpp"
+
+namespace dwmstatus {
+
+class pfile
+{
+public:
+    explicit
+    pfile(const string& filename)
+    {
+        f.rdbuf()->pubsetbuf(nullptr, 0);
+        f.exceptions(ifstream::failbit);
+        f.open(filename);
+    }
+
+    pfile(const pfile&) = delete;
+
+    pfile(pfile&&) = default;
+
+    pfile&
+    operator=(const pfile&) = delete;
+
+    pfile&
+    operator=(pfile&&) = default;
+
+    template <class T>
+    T
+    read()
+    {
+        f.seekg(0);
+        T ans;
+        f >> ans;
+        return ans;
+    }
+
+private:
+    ifstream f;
+};
+
+} // namespace dwmstatus
