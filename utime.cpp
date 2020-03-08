9e3196190a9f04311b24554c7afdb1b17f95ea77
Anton Yabchinskiy <arn@bestmx.net>
2020-03-08 14:17:02 +0300

    utime: init with braces

--- utime.cpp
+++ utime.cpp
@@ -21,7 +21,7 @@ read_pattern(istream& config)
 
 utime_status::utime_status(const string& pattern)
 {
-    UErrorCode err = U_ZERO_ERROR;
+    UErrorCode err{U_ZERO_ERROR};
     fmt.reset(new icu::SimpleDateFormat(icu::UnicodeString::fromUTF8(pattern), err));
     if (U_FAILURE(err)) {
         throw runtime_error(u_errorName(err));
@@ -38,8 +38,8 @@ utime_status::update(const system_clock::time_point& t)
 {
     icu::UnicodeString ustr;
     string str;
-    const auto t2 = system_clock::to_time_t(t);
-    const UDate t3 = 1E3 * static_cast<UDate>(t2);
+    const auto t2{system_clock::to_time_t(t)};
+    const UDate t3{1E3 * static_cast<UDate>(t2)};
     return fmt->format(t3, ustr).toUTF8String(str);
 }
 
