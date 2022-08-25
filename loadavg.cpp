c49465901bc83bdb13808fe2f8a56d6cd621cabf
Anton Yabchinskiy <arn@bestmx.net>
2022-08-25 21:10:44 +0300

    loadavg: formatting, rename output variable

--- loadavg.cpp
+++ loadavg.cpp
@@ -9,12 +9,16 @@ using std::ios_base;
 string loadavg_status::update(system_clock::time_point)
 {
     double loadavg[3] { 0.0, 0.0, 0.0 };
+
     getloadavg(loadavg, 3);
-    ostringstream out;
-    out.precision(2);
-    out.setf(ios_base::fixed);
-    out << loadavg[0] << ' ' << loadavg[1] << ' ' << loadavg[2];
-    return out.str();
+
+    ostringstream output;
+
+    output.precision(2);
+    output.setf(ios_base::fixed);
+    output << loadavg[0] << ' ' << loadavg[1] << ' ' << loadavg[2];
+
+    return output.str();
 }
 
 } // namespace dwmstatus
