4b6b9a3e105d45f82ac389b5028b4118c96e458e
Anton Yabchinskiy <arn@bestmx.net>
2022-08-25 21:01:05 +0300

    blocks: format with clang-format

--- blocks.cpp
+++ blocks.cpp
@@ -2,12 +2,11 @@
 
 namespace dwmstatus {
 
-const vector<string> horiz_blocks{ " ", "▏", "▎", "▍", "▌", "▋", "▊", "▉", "█" };
-const vector<string> shade_blocks{ " ", "░", "▒", "▓", "█" };
-const vector<string> vert_blocks{ " ", "▁", "▂", "▃", "▅", "▆", "▇", "█" };
+const vector<string> horiz_blocks { " ", "▏", "▎", "▍", "▌", "▋", "▊", "▉", "█" };
+const vector<string> shade_blocks { " ", "░", "▒", "▓", "█" };
+const vector<string> vert_blocks { " ", "▁", "▂", "▃", "▅", "▆", "▇", "█" };
 
-string
-choose_block(const vector<string>& table, double pct)
+string choose_block(const vector<string>& table, double pct)
 {
     if (pct < 0.0) {
         pct = 0.0;
--- blocks.hpp
+++ blocks.hpp
@@ -9,9 +9,8 @@ extern const vector<string> horiz_blocks;
 extern const vector<string> shade_blocks;
 extern const vector<string> vert_blocks;
 
-string
-choose_block(const vector<string>& table, double pct);
+string choose_block(const vector<string>& table, double pct);
 
 } // namespace dwmstatus
 
-#endif  // DWMSTATUS_BLOCKS_HPP
+#endif // DWMSTATUS_BLOCKS_HPP
