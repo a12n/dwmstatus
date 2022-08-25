1f87f84c450f0621a959e18640a1f184426c7ecc
Anton Yabchinskiy <arn@bestmx.net>
2022-08-25 23:49:40 +0300

    Linux/cputemp: format with clang-format

--- Linux/cputemp.cpp
+++ Linux/cputemp.cpp
@@ -1,20 +1,20 @@
-#include "config.hpp"
 #include "cputemp.hpp"
+
+#include "config.hpp"
 
 namespace dwmstatus {
 
-cputemp_status::cputemp_status(unsigned int i) :
-    hwmon{"/sys/class/hwmon/hwmon" + to_string(i) + "/temp1_input"}
+cputemp_status::cputemp_status(unsigned int i)
+    : hwmon { "/sys/class/hwmon/hwmon" + to_string(i) + "/temp1_input" }
 {
 }
 
-cputemp_status::cputemp_status(istream& config) :
-    cputemp_status(read_value<unsigned int>(config).value_or(0))
+cputemp_status::cputemp_status(istream& config)
+    : cputemp_status(read_value<unsigned int>(config).value_or(0))
 {
 }
 
-string
-cputemp_status::update(system_clock::time_point)
+string cputemp_status::update(system_clock::time_point)
 {
     return to_string(static_cast<int>(hwmon.rewind().read_value<double>().value_or(0.0) / 1000.0)) + " °C";
 }
--- Linux/cputemp.hpp
+++ Linux/cputemp.hpp
@@ -6,22 +6,17 @@
 
 namespace dwmstatus {
 
-class cputemp_status : public status_impl
-{
+class cputemp_status : public status_impl {
 public:
-    explicit
-    cputemp_status(unsigned int i = 0);
+    explicit cputemp_status(unsigned int i = 0);
 
-    explicit
-    cputemp_status(istream& config);
+    explicit cputemp_status(istream& config);
 
     cputemp_status(const cputemp_status&) = delete;
 
-    cputemp_status&
-    operator=(const cputemp_status&) = delete;
+    cputemp_status& operator=(const cputemp_status&) = delete;
 
-    virtual string
-    update(system_clock::time_point t) override;
+    virtual string update(system_clock::time_point t) override;
 
 private:
     pfile hwmon;
@@ -29,4 +24,4 @@ private:
 
 } // namespace dwmstatus
 
-#endif  // DWMSTATUS_LINUX_CPUTEMP_HPP
+#endif // DWMSTATUS_LINUX_CPUTEMP_HPP
