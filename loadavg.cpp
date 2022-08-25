5f66daffe617c8a19b8baea13458dc485aac828d
Anton Yabchinskiy <arn@bestmx.net>
2022-08-25 21:09:40 +0300

    loadavg: rename loadavg array

--- loadavg.cpp
+++ loadavg.cpp
@@ -8,13 +8,12 @@ using std::ios_base;
 
 string loadavg_status::update(system_clock::time_point)
 {
-    double ans[3] { 0.0, 0.0, 0.0 };
-
-    getloadavg(ans, 3);
+    double loadavg[3] { 0.0, 0.0, 0.0 };
+    getloadavg(loadavg, 3);
     ostringstream out;
     out.precision(2);
     out.setf(ios_base::fixed);
-    out << ans[0] << ' ' << ans[1] << ' ' << ans[2];
+    out << loadavg[0] << ' ' << loadavg[1] << ' ' << loadavg[2];
     return out.str();
 }
 
