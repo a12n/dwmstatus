b32ffc926f3fbeb3f2490903c2c08a1d6c98a091
Anton Yabchinskiy <arn@bestmx.net>
2022-08-28 02:49:36 +0300

    main: shorter names in main

--- main.cpp
+++ main.cpp
@@ -11,19 +11,18 @@ using std::cin;
 int main()
 {
 #ifdef DWMSTATUS_WITH_X11
-    x11_display display;
+    x11_display dpy;
 #else // DWMSTATUS_WITH_X11
-    term_display display;
+    term_display dpy;
 #endif // DWMSTATUS_WITH_X11
 
-    auto& conf = cin;
-    auto status = make_status_list(conf);
+    auto s = make_status_list(cin);
 
     while (true) {
         const auto t = system_clock::now();
 
-        if (update_status_list(status, t)) {
-            display.set_status(" " + format_status_list(status, " • ") + " ");
+        if (update_status_list(s, t)) {
+            dpy.set_status(" " + format_status_list(s, " • ") + " ");
         }
 
         this_thread::sleep_until(t + seconds(1));
