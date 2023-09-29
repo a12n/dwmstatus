7f30cac6ad298245005ac2fba9ed01f320d81286
Anton Yabchinskiy <arn@bestmx.net>
2023-09-30 00:20:30 +0300

    Linux/cputemp: remove

--- status.cpp
+++ status.cpp
@@ -1,15 +1,12 @@
 #include "status.hpp"
 
-#include "cputemp.hpp"
 #include "wlan.hpp"
 
 namespace dwmstatus {
 
 unique_ptr<status_impl> make_status_impl(string_view id, istream& conf)
 {
-    if (id == "cputemp") {
-        return make_unique<cputemp_status>(conf);
-    } else if (id == "wlan") {
+    if (id == "wlan") {
         return make_unique<wlan_status>(conf);
     } else {
         throw runtime_error("unknown status identifier");
